5c3f8898b3260372430b9ea731b406a3a558e6c1
John Hair <jhair1@umbc.edu>
2023-04-25 23:34:10 -0400

    Starting the main file

--- basicNet.py
+++ basicNet.py
@@ -14,3 +14,15 @@ if __name__ == "__main__":
     circuit.V("input", "in", circuit.gnd, 10@u_V)
     circuit.R(1, "in", "out", 9@u_kOhm)
     circuit.R(2, "out", circuit.gnd, 1@u_kOhm)
+
+    simulator = circuit.simulator(temperature=25, nominal_temperature=25)
+    print("The circuit/Netlist:\n\n", circuit)
+    
+    analysis = simulator.operating_point()
+
+    print(analysis.nodes['in'])
+    print(str(analysis.nodes['in']))
+    print(float(analysis.nodes['in']))
+    print(float(analysis.nodes['out']))
+
+    exit()
