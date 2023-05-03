b3ff6ab2ce8739a56cb6ccdf9e424069d5295bf0
John Hair <jhair1@umbc.edu>
2023-05-03 19:44:34 -0400

    Started copying over the RC and RL circuits

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
+# Usual libraries
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+
+# Schemdraw
 import schemdraw
 import schemdraw.elements as elm
+
+# PySpice Logging
+import PySpice.Logging.Logging as Logging
+logger = Logging.setup_logging()
+
+# Needed for circuit netlists
+from PySpice.Spice.Netlist import Circuit
+from PySpice.Unit import *
+from PySpice.Probe.Plot import plot
 
 def createNetlist(r1, r2, device):
     pass
@@ -73,17 +88,117 @@ def constructCircuit():
 
     return r1, r2, deviceVal
 
+def voltageDivider():
+    circuit = Circuit('Voltage Divider')
+
+    circuit.V('input', 1, circuit.gnd, 10@u_V)
+    circuit.R(1, 1, 2, 2@u_kΩ)
+    circuit.R(2, 2, circuit.gnd, 1@u_kΩ)
+
+    simulator = circuit.simulator(temperature=25, nominal_temperature=25)
+    analysis = simulator.operating_point()
+
+    for node in analysis.nodes.values():
+        print('Node {}: {:5.2f} V'.format(str(node), float(node)))
+
+def RCCircuit():
+    figure, (ax1, ax2) = plt.subplots(2, figsize=(20, 10))
+
+    element_types = ('capacitor')
+
+    for element_type in ('capacitor'):
+
+        circuit = Circuit(element_type.title())
+        # Fixme: compute value
+        source = circuit.PulseVoltageSource('input', 'in', circuit.gnd,
+                            initial_value=0@u_V, pulsed_value=10@u_V,
+                            pulse_width=10@u_ms, period=20@u_ms)
+        circuit.R(1, 'in', 'out', 1@u_kΩ)
+        if element_type == 'capacitor':
+            element = circuit.C
+            value = 1@u_uF
+            # tau = RC = 1 ms
+        else:
+            element = circuit.L
+            # Fixme: force component value to an Unit instance ?
+            value = 1@u_H
+            # tau = L/R = 1 ms
+        element(1, 'out', circuit.gnd, value)
+        # circuit.R(2, 'out', circuit.gnd, kilo(1)) # for debug
+
+        if element_type == 'capacitor':
+            tau = circuit['R1'].resistance * circuit['C1'].capacitance
+        else:
+            tau = circuit['L1'].inductance / circuit['R1'].resistance
+
+        simulator = circuit.simulator(temperature=25, nominal_temperature=25)
+        step_time = 10@u_us
+        analysis = simulator.transient(step_time=step_time, end_time=source.period*3)
+
+        # Let define the theoretical output voltage.
+        if element_type == 'capacitor':
+            def out_voltage(t, tau):
+                # Fixme: TypeError: only length-1 arrays can be converted to Python scalars
+                return float(source.pulsed_value) * (1 -  np.exp(-t / tau))
+        else:
+            def out_voltage(t, tau):
+                return float(source.pulsed_value) * np.exp(-t / tau)
+        # Fixme: get step_time from analysis
+        # At t = 5 tau, each circuit has nearly reached it steady state.
+        i_max = int(5 * tau / float(step_time))
+        popt, pcov = curve_fit(out_voltage, analysis.out.abscissa[:i_max], analysis.out[:i_max])
+        tau_measured = popt[0]
+
+        # Fixme: use Unit().canonise()
+        print('tau {0} = {1}'.format(element_type, tau.canonise().str_space()))
+        print('tau measured {0} = {1:.1f} ms'.format(element_type, tau_measured * 1000))
+
+        if element_type == 'capacitor':
+            ax = ax1
+            title = "Capacitor: voltage is constant"
+        else:
+            ax = ax2
+            title = "Inductor: current is constant"
+        ax.set_title(title)
+        ax.grid()
+        current_scale = 1000
+        ax.plot(analysis['in'])
+        ax.plot(analysis['out'])
+        # Fixme: resistor current, scale
+        ax.plot(((analysis['in'] - analysis.out)/circuit['R1'].resistance) * current_scale)
+        ax.axvline(x=float(tau), color='red')
+        ax.set_ylim(-11, 11)
+        ax.set_xlabel('t [s]')
+        ax.set_ylabel('[V]')
+        ax.legend(('Vin [V]', 'Vout [V]', 'I'), loc=(.8,.8))
+
+    plt.tight_layout()
+    plt.show()
+
+def RLCircuit():
+    pass
+
 def start():
     choice = 0
-    while choice != 2:
+    while choice != 5:
         print("What would would you like to do?")
-        print("1. Construct circuit")
-        print("2. Quit")
+        print("1. Construct main circuit")
+        print("2. Simulate voltage divider")
+        print("3. Simulate RC circuit")
+        print("4. Simulate RL circuit")
+        print("5. Quit")
         choice = int(input("Choice: "))
-        if choice == 2:
+        if choice == 1:
+            r1, r2, deviceVal = constructCircuit()
+            createNetlist(r1, r2, deviceVal)
+        elif choice == 2:
+            voltageDivider()
+        elif choice == 3:
+            RCCircuit()
+        elif choice == 4:
+            RLCircuit()
+        elif choice == 5:
             break
-        r1, r2, deviceVal = constructCircuit()
-        createNetlist(r1, r2, deviceVal)
 
 if __name__ == "__main__":
     start()
--- test.py
+++ test.py
@@ -1,69 +1,86 @@
-def battery():
-    
-    print("Now...which direction is the new battery (B2) pointing with respect to the original battery (B1)?")
-    print("")
-    print("As if our new battery was in series, use the menu to decide the direction.")
-    print("")
-    print("MENU--1: same direction; 2: opposite direction.")
-    
-#def capacitor():
-#def inductor():
-def resistor():
-    
-    Vb = float(input("What is the voltage of B1 in Volts: "))
-    strength_1 = float(input("What is the resistance of R1 in Ohms: "))
-    strength_2 = float(input("What is the resistance of R2 in Ohms: "))
-    i_R1 = float(Vb/strength_1)
-    print("")
-    print("Although R2 is",strength_2,"Ohms, it has no impact on the current through R1 (i_R1), because the resistors are in parallel.")
-    print("")
-    print("Kirchoff's Voltage Rule demands the voltage of the battery (Vb) must equal the sum of voltage drops across any path of current.")
-    print("")
-    print("We ONLY have two objects in this circuit, and they are in parallel, so the voltage drop across each equals Vb.")
-    print("")
-    return i_R1
+import numpy as np
+import matplotlib.pyplot as plt
 
-if __name__ == '__main__':
-    
-    print("There is a simple circuit with one battery (B1) and two items in parallel. There is nothing else outside the parallel.")
-    print("")
-    print("Occupying one route is a resistor (R1). Using the menu, you get to choose the item across the resistor!")
-    print("")
-    print("Then the program will calculate the current flowing through the resistor as a function of time.")
-    print("")
-    print("As you can imagine, depending on what you choose...the current will behave differently.")
-    print("")
-    print("MENU--1: Battery; 2: Capacitor; 3: Inductor; 4: Resistor; 5: Just Wire")
-    
-    item_block = int(input("Enter a number on the menu: "))
-    
-    #if item_block == 1:
-        
-    #if item_block == 2:
-    
-    #if item_block == 3:
-        
-    if item_block == 4:
-        print("The current across R1 is",resistor(),"Amperes.")
-        print("")
-        print("Current is a dependent variable with respect to voltage and resistance.")
-        print("")
-        print("To change i_R1, either change R1, add a resistor in series with R1, or change Vb.")
-        
-    if item_block == 5:
-        print("")
-        print("The current across R1 is 0 Amperes.")
-        print("")
-        print("Kirchoff's Junction Rule demands the current that enters a parallel must be equal to the current that exits it.")
-        print("")
-        print("This means the sum of the currents through each loop of the parallel equals the total current of the circuit.")
-        print("")
-        print("However, when encountering a parallel, the majority of the current will feed through the loop with the least resistance.")
-        print("")
-        print("Ultimately, both loops will have current flow (reduced with respect to the total circuit current) unless...")
-        print("")
-        print("One of the paths has NO resistance, so ALL of the current will feed through that loop, leaving i_R1 = 0.")
-        
+
+import PySpice.Logging.Logging as Logging
+logger = Logging.setup_logging()
+
+
+from PySpice.Probe.Plot import plot
+from PySpice.Spice.Netlist import Circuit
+from PySpice.Unit import *
+
+from scipy.optimize import curve_fit
+
+figure, (ax1, ax2) = plt.subplots(2, figsize=(20, 10))
+
+element_types = ('capacitor', 'inductor')
+
+for element_type in ('capacitor', 'inductor'):
+
+    circuit = Circuit(element_type.title())
+    # Fixme: compute value
+    source = circuit.PulseVoltageSource('input', 'in', circuit.gnd,
+                           initial_value=0@u_V, pulsed_value=10@u_V,
+                           pulse_width=10@u_ms, period=20@u_ms)
+    circuit.R(1, 'in', 'out', 1@u_kΩ)
+    if element_type == 'capacitor':
+        element = circuit.C
+        value = 1@u_uF
+        # tau = RC = 1 ms
     else:
-        print("Invalid input. Rerun the program.")
-        print("test commit")
+        element = circuit.L
+        # Fixme: force component value to an Unit instance ?
+        value = 1@u_H
+        # tau = L/R = 1 ms
+    element(1, 'out', circuit.gnd, value)
+    # circuit.R(2, 'out', circuit.gnd, kilo(1)) # for debug
+
+    if element_type == 'capacitor':
+        tau = circuit['R1'].resistance * circuit['C1'].capacitance
+    else:
+        tau = circuit['L1'].inductance / circuit['R1'].resistance
+
+    simulator = circuit.simulator(temperature=25, nominal_temperature=25)
+    step_time = 10@u_us
+    analysis = simulator.transient(step_time=step_time, end_time=source.period*3)
+
+    # Let define the theoretical output voltage.
+    if element_type == 'capacitor':
+        def out_voltage(t, tau):
+            # Fixme: TypeError: only length-1 arrays can be converted to Python scalars
+            return float(source.pulsed_value) * (1 -  np.exp(-t / tau))
+    else:
+        def out_voltage(t, tau):
+            return float(source.pulsed_value) * np.exp(-t / tau)
+    # Fixme: get step_time from analysis
+    # At t = 5 tau, each circuit has nearly reached it steady state.
+    i_max = int(5 * tau / float(step_time))
+    popt, pcov = curve_fit(out_voltage, analysis.out.abscissa[:i_max], analysis.out[:i_max])
+    tau_measured = popt[0]
+
+    # Fixme: use Unit().canonise()
+    print('tau {0} = {1}'.format(element_type, tau.canonise().str_space()))
+    print('tau measured {0} = {1:.1f} ms'.format(element_type, tau_measured * 1000))
+
+    if element_type == 'capacitor':
+        ax = ax1
+        title = "Capacitor: voltage is constant"
+    else:
+        ax = ax2
+        title = "Inductor: current is constant"
+    ax.set_title(title)
+    ax.grid()
+    current_scale = 1000
+    ax.plot(analysis['in'])
+    ax.plot(analysis['out'])
+    # Fixme: resistor current, scale
+    ax.plot(((analysis['in'] - analysis.out)/circuit['R1'].resistance) * current_scale)
+    ax.axvline(x=float(tau), color='red')
+    ax.set_ylim(-11, 11)
+    ax.set_xlabel('t [s]')
+    ax.set_ylabel('[V]')
+    ax.legend(('Vin [V]', 'Vout [V]', 'I'), loc=(.8,.8))
+
+plt.tight_layout()
+plt.show()
