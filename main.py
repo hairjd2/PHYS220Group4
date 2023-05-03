b3d39739328c7f89049ba9f638888b4ee79eb523
John Hair <jhair1@umbc.edu>
2023-05-03 19:52:36 -0400

    Got individual graph for RC circuit

--- main.py
+++ main.py
@@ -102,7 +102,7 @@ def voltageDivider():
         print('Node {}: {:5.2f} V'.format(str(node), float(node)))
 
 def RCCircuit():
-    figure, (ax1, ax2) = plt.subplots(2, figsize=(20, 10))
+    # figure, (ax1, ax2) = plt.subplots(2, figsize=(20, 10))
 
     element_types = ('capacitor')
 
@@ -114,35 +114,23 @@ def RCCircuit():
                             initial_value=0@u_V, pulsed_value=10@u_V,
                             pulse_width=10@u_ms, period=20@u_ms)
         circuit.R(1, 'in', 'out', 1@u_kΩ)
-        if element_type == 'capacitor':
-            element = circuit.C
-            value = 1@u_uF
-            # tau = RC = 1 ms
-        else:
-            element = circuit.L
-            # Fixme: force component value to an Unit instance ?
-            value = 1@u_H
+
+        element = circuit.C
+        value = 1@u_uF
             # tau = L/R = 1 ms
         element(1, 'out', circuit.gnd, value)
         # circuit.R(2, 'out', circuit.gnd, kilo(1)) # for debug
 
-        if element_type == 'capacitor':
-            tau = circuit['R1'].resistance * circuit['C1'].capacitance
-        else:
-            tau = circuit['L1'].inductance / circuit['R1'].resistance
+        tau = circuit['R1'].resistance * circuit['C1'].capacitance
 
         simulator = circuit.simulator(temperature=25, nominal_temperature=25)
         step_time = 10@u_us
         analysis = simulator.transient(step_time=step_time, end_time=source.period*3)
 
         # Let define the theoretical output voltage.
-        if element_type == 'capacitor':
-            def out_voltage(t, tau):
+        def out_voltage(t, tau):
                 # Fixme: TypeError: only length-1 arrays can be converted to Python scalars
-                return float(source.pulsed_value) * (1 -  np.exp(-t / tau))
-        else:
-            def out_voltage(t, tau):
-                return float(source.pulsed_value) * np.exp(-t / tau)
+            return float(source.pulsed_value) * (1 -  np.exp(-t / tau))
         # Fixme: get step_time from analysis
         # At t = 5 tau, each circuit has nearly reached it steady state.
         i_max = int(5 * tau / float(step_time))
@@ -153,24 +141,19 @@ def RCCircuit():
         print('tau {0} = {1}'.format(element_type, tau.canonise().str_space()))
         print('tau measured {0} = {1:.1f} ms'.format(element_type, tau_measured * 1000))
 
-        if element_type == 'capacitor':
-            ax = ax1
-            title = "Capacitor: voltage is constant"
-        else:
-            ax = ax2
-            title = "Inductor: current is constant"
-        ax.set_title(title)
-        ax.grid()
+        title = "Capacitor: voltage is constant"
+        plt.title(title)
+        plt.grid()
         current_scale = 1000
-        ax.plot(analysis['in'])
-        ax.plot(analysis['out'])
+        plt.plot(analysis['in'])
+        plt.plot(analysis['out'])
         # Fixme: resistor current, scale
-        ax.plot(((analysis['in'] - analysis.out)/circuit['R1'].resistance) * current_scale)
-        ax.axvline(x=float(tau), color='red')
-        ax.set_ylim(-11, 11)
-        ax.set_xlabel('t [s]')
-        ax.set_ylabel('[V]')
-        ax.legend(('Vin [V]', 'Vout [V]', 'I'), loc=(.8,.8))
+        plt.plot(((analysis['in'] - analysis.out)/circuit['R1'].resistance) * current_scale)
+        plt.axvline(x=float(tau), color='red')
+        plt.ylim(-11, 11)
+        plt.xlabel('t [s]')
+        plt.ylabel('[V]')
+        plt.legend(('Vin [V]', 'Vout [V]', 'I'), loc=(.8,.8))
 
     plt.tight_layout()
     plt.show()
